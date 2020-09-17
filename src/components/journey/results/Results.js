ab2047930cc64973f2346f45978480fc4d20fb46
wbglaeser <ben.glaeser@tuta.io>
2020-09-17 17:31:09 +0200

    match results to provided answers

--- src/components/journey/results/Results.js
+++ src/components/journey/results/Results.js
@@ -3,6 +3,11 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
+import Accordion from "./ResultsInfobox.js";
+
+import result_map from "./../documents/resultmap.json";
+import result_specs from "./../documents/resultspecs.json";
+
 const useStyles = makeStyles((theme) => ({
   mainSpace: {
       backgroundColor: theme.palette.background.paper,
@@ -15,17 +20,47 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+const isTrue = (bool) => bool===true;
+
+
+const initialiseDocumentQueue = () => {
+  return result_specs.filter(function (element) {
+    return element.type === "default"
+  })
+}
+
 export default function Result(props) {
   const classes = useStyles();
 
+  const matchResultType = () => {
+    let answers_keys = Object.keys(props.answers);
+    let result_match = null;
+    for (var i=0; i < result_map.length; i++) {
+      for (var y=0; y<answers_keys.length; y++) {
+        if (result_map[i]["input"][answers_keys[y]] != props.answers[answers_keys[y]][0]) { break; }
+        else { result_match = result_map[i]["identifier"] }
+      }
+    }
+    return result_match
+  }
+
+  const retrieveResultSpecs = () => {
+    let result_identifier = matchResultType();
+    let res_document = result_specs.filter(function(el) {
+      return el.identifier == result_identifier
+    })
+    return res_document[0]
+  }
+
+  let resDoc = retrieveResultSpecs();
+  console.log(resDoc);
+
   return (
     <Grid container className={classes.mainSpace}>
-      <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+      <Typography component="h1" variant="h4" align="center" color="textPrimary" gutterBottom>
         Ergebnis
       </Typography>
-      <Typography variant="h5" align="center" color="textSecondary" paragraph>
-        Hier wird die ratsuchende über ihre möglichkeiten aufgeklärt.
-      </Typography>
+      <Accordion />
     </Grid>
   );
 }
