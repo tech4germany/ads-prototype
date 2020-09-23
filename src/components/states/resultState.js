3ba18d5a0c5dc08a89ed1413abc032edba6d6279
wbglaeser <ben.glaeser@tuta.io>
2020-09-23 18:29:19 +0200

    add unit tests for resultState

--- src/components/states/resultState.js
+++ src/components/states/resultState.js
@@ -4,30 +4,48 @@ import { useState } from 'react';
 import result_map from "./../journey/documents/resultmap.json";
 import result_specs from "./../journey/documents/resultspecs.json";
 
-let matchAnswer = (answers) => {
-  let answers_keys = answers.keys()
-  let res_match = -1;
-  for (var i=0; i < result_map.length; i++) {
-    let kill = 0;
-    for (var y=0; y<answers_keys.length; y++) {
-      try {
-        if (!(result_map[i]["input"][answers_keys[y]].includes(answers.self[answers_keys[y]][0]))) {
-          kill = 1;
-          break;
-        }
-      } catch(err) { continue }
-    }
-    if (kill===1) { continue; }
-    else { res_match = result_map[i]["identifier"] }
-  }
-  return res_match
-}
+const currentExceptions = [
+  "merkmal_ethnisch_detail",
+  "merkmal_religion_detail",
+  "merkmal_behinderung_detail",
+  "merkmal_sex_detail",
+  "lebensbereich_arbeit_detail",
+  "lebensbereich_behörden_detail",
+  "lebensbereich_wohnen_detail",
+  "lebensbereich_bildung_detail",
+  "lebensbereich_gesundheit_detail"
+  ]
 
-function useResultSpecs(initialState = -1) {
+export function useResultSpecs(initialState = -1) {
   let [self, setResultSpecs] = useState(initialState)
 
+  let _splitAnswers = (answers) => {
+    return [answers.self, answers.keys()]
+  }
+
+  let matchAnswer = (answers_vals, answers_keys) => {
+    let res_match = -1;
+    for (var i=0; i < result_map.length; i++) {
+      let kill_switch = 1;
+      for (var y=0; y<answers_keys.length; y++) {
+        kill_switch = 0;
+        if (!currentExceptions.includes(answers_keys[y])) {
+          if (!(result_map[i]["input"][answers_keys[y]].includes(answers_vals[answers_keys[y]][0]))) {
+            kill_switch = 1;
+            break;
+          } else { continue; }
+        }
+      }
+      if (kill_switch===1) { continue; }
+      else { res_match = result_map[i]["identifier"] }
+    }
+    return res_match
+  }
+
   let retrieveSpecs = (answers) => {
-    let res_match = matchAnswer(answers);
+    let answers_vals = _splitAnswers(answers)[0]
+    let answers_keys = _splitAnswers(answers)[1]
+    let res_match = matchAnswer(answers_vals, answers_keys);
     let res_document = result_specs.filter(function(el) {
       return el.identifier === res_match
     })
@@ -35,6 +53,6 @@ function useResultSpecs(initialState = -1) {
     else {return {}}
   }
 
-  return { self, retrieveSpecs }
+  return { self, retrieveSpecs, matchAnswer}
 }
 export const ResultSpecs = createContainer(useResultSpecs)
