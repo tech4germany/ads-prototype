f52efe76bf58ce5b78f04e78140c4f0f8a7f2bdd
wbglaeser <ben.glaeser@tuta.io>
2020-09-30 16:49:38 +0200

    setup typescript compiler config

--- src/components/journey/JourneyNavigation.js
+++ src/components/journey/JourneyNavigation.js
@@ -3,7 +3,7 @@ import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
-import { NextButton, BackButton } from "components/journey/JourneyButtons.js"
+import { NextButton, BackButton } from "components/journey/JourneyButtons.tsx"
 import Stepper from "components/journey/JourneyStepper.js"
 
 import { ActiveStep } from "components/states/activeStepState.js";
