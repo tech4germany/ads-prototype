81a2bd92a91361334d169c556040ac50b40cdca4
wbglaeser <ben.glaeser@tuta.io>
2020-10-04 15:39:57 +0200

    pull standard style patterns out of components

---
+++ src/styleguide.ts
@@ -0,0 +1,25 @@
+export const colorMain = {
+  "115":"#f3b500",
+  "100":'#ffe57f',
+  "45": "#ffecb3",
+  "15":"#fef3df",
+}
+
+export const textSelectionMain = {
+  "fontFamily": "BundesSansWeb-Bold",
+  "fontSize": "1.8vh",
+  "color": {
+    "active": "white",
+    "inactive": "black",
+  }
+}
+
+export const questionHeader = {
+  "fontFamily": "BundesSansWeb-Bold",
+  "fontSize": "4vh",
+}
+
+export const questionExplanation = {
+  "fontFamily": "BundesSansWeb-Regular",
+  "fontSize": "2vh",
+}
