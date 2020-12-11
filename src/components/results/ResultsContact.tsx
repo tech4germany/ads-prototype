e121ac5235c396c69ba7177a1b0dd08a31f5bdce
wbglaeser <ben.glaeser@tuta.io>
2020-12-11 12:12:56 +0100

    update text in frist tile and mailto link

--- src/components/results/ResultsContact.tsx
+++ src/components/results/ResultsContact.tsx
@@ -88,7 +88,7 @@ export default function ResultContact() {
             <h3 className={classes.contactHeaderText}>Kontakt zu uns</h3>
           </div>
           <div className={classes.contactInfoContainer}>
-            <p className={classes.contactInfoText}>Für eine Beratung oder allgemeine Fragen nutzen Sie unser Kontaktformular oder schreiben eine Email an: <a className={classes.emailLink} href={"email:beratung@ads.bund.de"}>beratung@ads.bund.de</a></p>
+            <p className={classes.contactInfoText}>Für eine Beratung oder allgemeine Fragen nutzen Sie unser Kontaktformular oder schreiben eine Email an: <a className={classes.emailLink} href={"mailto:beratung@ads.bund.de"}>beratung@ads.bund.de</a></p>
             <span className={classes.contactSpacer}></span>
             <button onClick={(e) => {e.preventDefault(); window.location.href="https://www.antidiskriminierungsstelle.de/DE/Beratung/Beratung_Moeglichkeiten/Formular/Formular1_node.html";}}
             className={classes.contactButton}><a title="Zum Kontaktformular" tabIndex={0} className={classes.contactFormularText} href={"https://www.antidiskriminierungsstelle.de/DE/Beratung/Beratung_Moeglichkeiten/Formular/Formular1_node.html"}>Zum Kontaktformular</a></button>
